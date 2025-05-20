cc57e795eae6f09d7c073dea870f5636699e964d
IlyaEdelshtein <131966327+IlyaEdelshtein@users.noreply.github.com>
2025-05-20 14:08:39 +0300

    Add animated clock to Page Two

--- src/pages/PageTwo.tsx
+++ src/pages/PageTwo.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
 
-const PageTwo: React.FC = () => <div>Page Two</div>;
+const PageTwo: React.FC = () => {
+  const [time, setTime] = React.useState(new Date());
+  const [position, setPosition] = React.useState({ top: 0, left: 0 });
+
+  React.useEffect(() => {
+    const update = () => {
+      setTime(new Date());
+      setPosition({
+        top: Math.random() * (window.innerHeight - 50),
+        left: Math.random() * (window.innerWidth - 100),
+      });
+    };
+
+    update();
+    const interval = setInterval(update, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: position.top,
+        left: position.left,
+        transition: 'top 0.5s linear, left 0.5s linear',
+        fontSize: '2rem',
+      }}
+    >
+      {time.toLocaleTimeString()}
+    </div>
+  );
+};
 
 export default PageTwo;
